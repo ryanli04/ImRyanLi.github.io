3abe8f7bd505f72b3870ba1f573a2260430e1db1
Ryan Li <ryanli8999@gmail.com>
2024-06-02 23:40:36 -0400

    initial

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import cpp from "./tech/cpp.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+import java from "./tech/java.png";
+
+import retaildata from "./company/retaildata.png";
+import mes from "./company/mes.png"
+import peel from "./company/peel.png"
+import first from "./company/first.png"
+
+import airbnbclone from "./airbnbclone.png";
+import lidar from "./lidar.png";
+import pathfind from "./pathfind.png"
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  cpp,
+  docker,
+  figma,
+  python,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  java,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  airbnbclone,
+  lidar,
+  pathfind,
+  retaildata,
+  mes,
+  peel,
+  first
+};
---
+++ src/constants/index.js
@@ -0,0 +1,256 @@
+import { three } from "maath";
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    java,
+    typescript,
+    html,
+    css,
+    cpp,
+    python,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    docker,
+    airbnbclone,
+    lidar,
+    pathfind,
+    retaildata,
+    mes,
+    peel,
+    first
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "ReactJS Developer",
+      icon: mobile,
+    },
+    {
+      title: "Full Stack Developer",
+      icon: backend,
+    },
+    {
+      title: "Student",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Next JS",
+      icon: redux,
+    },
+    {
+      name: "C++",
+      icon: cpp,
+    },
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "Java",
+      icon: java,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Publications Editor",
+      company_name: "McMaster Engineering Society",
+      icon: mes,
+      iconBg: "#383E56",
+      date: "Sep 2023 - April 2024",
+      points: [
+        "Collaborated alongside engineering students to create informational newsletters with goals of increasing engagement in the engineering community.",
+        "Communicated with editorial and website committee to publish newsletters in a timely manner",
+      ],
+    },
+    {
+      title: "Research Associate",
+      company_name: "RetailData",
+      icon: retaildata,
+      iconBg: "#E6DEDD",
+      date: "Jun 2023 - Aug 2023",
+      points: [
+        "Presented research findings to senior management and clients, providing actionable recommendations based on data analysis.",
+        "Conducted market research and competitive analysis to evaluate the performance of retail products and strategies",
+      ],
+    },
+    {
+      title: "Head of Competition",
+      company_name: "Computer Science Club",
+      icon: peel,
+      iconBg: "#E6DEDD",
+      date: "Oct 2021 - Jun 2022",
+      points: [
+        "Created questions and solutions to practice for the Canadian Computing Competition (CCC) using both Python and Java.",
+        "Collaborated with team members to teach a class of 30+ students about programming fundamentals.",
+        "Used problem-solving skills to guide students through solutions."
+      ],
+    },
+    {
+      title: "Software/Electrical Sub-Team",
+      company_name: "FIRST Robotics 1241",
+      icon: first,
+      iconBg: "#E6DEDD",
+      date: "Sep 2021 - Jun 2022",
+      points: [
+        "Assisted in the wiring and coding components of the robot alongside numerous team members.",
+        "Operated machines to help create physical components of the robot."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Airbnb Clone",
+      description:
+        "Web-based platform that mocks the actions of Airbnb. This application allows users to search, book, create, and manage home rentals from various providers.",
+      tags: [
+        {
+          name: "next.js",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: airbnbclone,
+      source_code_link: "https://github.com/ryanli04/Airbnb-clone",
+    },
+    {
+      name: "LiDAR-Scanner",
+      description:
+        "A simple 3D LiDAR Scanner using a ToF sensor, a stepper motor, and a microcontroller. The set up also includes an externally wired push button.",
+      tags: [
+        {
+          name: "c",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "assembly",
+          color: "green-text-gradient",
+        },
+        {
+          name: "python",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: lidar,
+      source_code_link: "https://github.com/ryanli04/LiDAR-Scanner",
+    },
+    {
+      name: "A* Pathfinding",
+      description:
+        "A* search algorithm path finding visualizer using python.",
+      tags: [
+        {
+          name: "python",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: pathfind,
+      source_code_link: "https://github.com/ryanli04/A-PathFinding",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
