96a48fa3e9b950dee4cb4f91dd2dc405fa9a3c3d
Ryan Li <ryanli8999@gmail.com>
2024-06-03 14:21:04 -0400

    Hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,8 +17,9 @@ const Hero = () => {
             <h1 className={`${styles.heroHeadText} text-white`}
             >Hi, I'm <span className='text-[#915eff]'>Ryan</span></h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop UI/UX designs and <br className='sm:block hidden'/>
-              web applications
+              I am a full-stack developer <br className='sm:block hidden'/>
+              with a focus on front-end <br className='sm:block hidden'/> 
+              development.
             </p>
           </div>
       </div>
